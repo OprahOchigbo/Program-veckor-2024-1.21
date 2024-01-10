00e4a2b7a9f9be564b4f78a7f8412ab97e6e0c8f
ItsMollyHere <sofhil2007@gmail.com>
2024-01-10 21:18:49 +0100

    wip movement

---
+++ Assets/playermovement.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playermovement : MonoBehaviour
+{
+    public float movespeed = 5f;
+
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

0559f180e445c795cacb7321e13648ca600aba37
OprahOchigbo <156102827+OprahOchigbo@users.noreply.github.com>
2024-01-12 21:23:54 +0100

    Update CollidableObject.cs

--- Assets/CollidableObject.cs
+++ Assets/CollidableObject.cs
@@ -7,8 +7,8 @@ public class CollidableObject : MonoBehaviour
     private Collider2D z_Collider;
     [SerializeField]
     private ContactFilter2D z_Filter;
-    private List<Collider2D> CollidedObject1;
-
+    private List<Collider2D> CollidedObject1 = new List<Collider2D>(1);
+    
     private void Start()
     {
       z_Collider = GetComponent<Collider2D>();

cb8b2c60a206e3b20f82186a5d5ce415a5dda534
OprahOchigbo <156102827+OprahOchigbo@users.noreply.github.com>
2024-01-12 21:18:34 +0100

    Colliables

---
+++ Assets/CollidableObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollidableObject : MonoBehaviour
+{
+    private Collider2D z_Collider;
+    [SerializeField]
+    private ContactFilter2D z_Filter;
+    private List<Collider2D> CollidedObject1;
+
+    private void Start()
+    {
+      z_Collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        z_Collider.OverlapCollider(z_Filter, CollidedObject1);
+        foreach (var o in CollidedObject1)
+        {
+            Debug.Log("Collided with" + o.name);
+        }
+    }
+    
+}

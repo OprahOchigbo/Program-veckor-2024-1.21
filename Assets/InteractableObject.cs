ccc50d56450d61850e7bb02a1e5b6bf88d728b6c
OprahOchigbo <156102827+OprahOchigbo@users.noreply.github.com>
2024-01-13 19:57:36 +0100

    Interactable script complete

--- Assets/InteractableObject.cs
+++ Assets/InteractableObject.cs
@@ -2,17 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InteractableObject : MonoBehaviour
+public class InteractableObject : CollidableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool z_Interacted = false;
 
-    // Update is called once per frame
-    void Update()
+    protected override void OnCollided(GameObject collidedObject)
     {
+        if (Input.GetKey(KeyCode.E))
+        {
+            OnInteract();
+        }
+    }
+    protected virtual void OnInteract()
+    {
+        if (!z_Interacted)
+        {
+            z_Interacted = true;
+            Debug.Log("Interact with " + name);
+        }
         
     }
 }

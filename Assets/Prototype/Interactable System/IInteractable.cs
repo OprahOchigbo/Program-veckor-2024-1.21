10fefa8320589ea17c0a3ab390522911ba6e1d10
OprahOchigbo <156102827+OprahOchigbo@users.noreply.github.com>
2024-01-17 14:40:53 +0100

    Interactions or whatever

--- Assets/Prototype/Interactable System/IInteractable.cs
+++ Assets/Prototype/Interactable System/IInteractable.cs
@@ -2,9 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public interface Interactable
 {
     public string InteractionPrompt { get; }
 
-    public void Interact (Interactor)
+    public bool Interact(Interactor Interactor);
 }

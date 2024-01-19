7bb0222572a9139e0c9cb618a216472695695875
OprahOchigbo <156102827+OprahOchigbo@users.noreply.github.com>
2024-01-19 12:20:13 +0100

    disclaimer stuff

--- Assets/Prototype/LevelChanger3.cs
+++ Assets/Prototype/LevelChanger3.cs
@@ -14,7 +14,7 @@ public class LevelChanger : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
+        if (Input.GetKeyDown(KeyCode.E))
         {
             SceneManager.LoadScene(3);
 

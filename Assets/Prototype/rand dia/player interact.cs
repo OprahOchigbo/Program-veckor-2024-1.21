5cca0dca09184f763043d088e888b7d2c7a815b0
ItsMollyHere <153902500+ItsMollyHere@users.noreply.github.com>
2024-02-08 14:50:13 +0100

    stuff for rando dialogue

---
+++ Assets/Prototype/rand dia/player interact.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerinteract : MonoBehaviour
+{
+    public random dialogueManager; 
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            StartRandomNPCDialogue();
+        }
+    }
+
+    void StartRandomNPCDialogue()
+    {
+        if (dialogueManager != null)
+        {
+            dialogueManager.StartRandomDialogue();
+        }
+        else
+        {
+            Debug.LogWarning("not attached :(");
+        }
+    }
+}
---
+++ Assets/Prototype/rand dia/random.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class random: MonoBehaviour
+{
+    public string[] dialogueLines;
+    void Start()
+    {
+        StartRandomDialogue();
+        
+    }
+
+    // Update is called once per frame
+    void StartRandomDialogue()
+    {
+        if(dialogueLines.Length > 0)
+        {
+            int randomIndex = Random.Range(0, dialogueLines.Length);
+
+            string randomDialogue = dialogueLines[randomIndex];
+
+            Debug.Log("NPC:" + randomDialogue);
+        }
+        else
+        {
+            Debug.LogWarning("No dialogue :(");
+        }
+    }
+}
